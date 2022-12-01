dca530b24e86a484b2375d0436f87506269bf8d6
CatMiaogu <1016489713@qq.com>
2022-12-01 09:18:56 +0800

    feat: add element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// 引入 element-plus 和index.css 样式文件
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 import router from './router'
 import store from './store'
 
 const app = createApp(App)
+app.use(ElementPlus)
 app.use(router)
 app.use(store)
 app.mount('#app')
